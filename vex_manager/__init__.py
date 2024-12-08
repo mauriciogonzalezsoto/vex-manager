7e8bf9b486a352a29c5a67cbec05de40d03f3ed7
Mauricio Gonzalez Soto <ommagoso10@gmail.com>
2024-12-07 22:50:29 -0800

    chore: disable debug logging mode

--- vex_manager/__init__.py
+++ vex_manager/__init__.py
@@ -1,6 +1,8 @@
 import logging
 
+from vex_manager.gui.vex_manager_ui import VEXManagerUI
+
 
 logging.basicConfig(format=f'%(levelname)s: [VEX Manager] %(message)s')
 logger = logging.getLogger('vex_manager')
-logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.DEBUG)
