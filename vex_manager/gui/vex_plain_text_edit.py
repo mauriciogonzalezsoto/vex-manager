5b39671147e43160774dcf5ad3502c1aefadcc29
Mauricio Gonzalez Soto <ommagoso10@gmail.com>
2024-12-13 21:53:08 -0800

    feat: implement delimiter autocompletion and automatic indentation

--- vex_manager/gui/vex_plain_text_edit.py
+++ vex_manager/gui/vex_plain_text_edit.py
@@ -2,7 +2,12 @@ from PySide2 import QtWidgets
 from PySide2 import QtCore
 from PySide2 import QtGui
 
+import logging
+
 from vex_manager.config import VEXSyntaxis
+
+
+logger = logging.getLogger(f'vex_manager.{__name__}')
 
 
 class VEXPlainTextEdit(QtWidgets.QPlainTextEdit):
@@ -10,16 +15,135 @@ class VEXPlainTextEdit(QtWidgets.QPlainTextEdit):
     def __init__(self) -> None:
         super().__init__()
 
+        self.tab_size = 4
+
         self.font = QtGui.QFont()
         self.font.setBold(True)
+        self.font.setWordSpacing(5)
 
         self.setFont(self.font)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.setWordWrapMode(QtGui.QTextOption.NoWrap)
 
         VEXSyntaxHighlighter(self.document())
 
+    def _handle_cursor_behavior(self, char: str) -> bool:
+        text_cursor = self.textCursor()
+        text_cursor_selected_text = text_cursor.selectedText()
+
+        if not text_cursor_selected_text:
+            cursor_position = text_cursor.position()
+
+            if cursor_position:
+                plain_text = self.toPlainText()
+
+                if cursor_position < len(plain_text):
+                    text_at_cursor = self.toPlainText()[cursor_position]
+
+                    if text_at_cursor == char:
+                        text_cursor = self.textCursor()
+                        text_cursor.movePosition(text_cursor.NextCharacter)
+                        self.setTextCursor(text_cursor)
+
+                        return True
+
+        return False
+
+    def _insert_matching_delimiter(self, delimiter: str) -> None:
+        if delimiter == '{':
+            matching_delimiter = '}'
+        elif delimiter == '(':
+            matching_delimiter = ')'
+        elif delimiter == '[':
+            matching_delimiter = ']'
+        elif delimiter == '"':
+            matching_delimiter = '"'
+        elif delimiter == '\'':
+            matching_delimiter = '\''
+        else:
+            matching_delimiter = ''
+
+        self.insertPlainText(delimiter)
+        self.insertPlainText(matching_delimiter)
+
+        text_cursor = self.textCursor()
+        text_cursor.movePosition(text_cursor.PreviousCharacter)
+        self.setTextCursor(text_cursor)
+
     def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
-        if event.key() == QtCore.Qt.Key_Tab:
-            self.insertPlainText('    ')
+        key = event.key()
+
+        text_cursor = self.textCursor()
+        text_cursor_selected_text = text_cursor.selectedText()
+        text_cursor_block = text_cursor.block()
+        current_line_text = text_cursor_block.text()
+
+        if key == QtCore.Qt.Key_Tab:
+            self.insertPlainText(''.ljust(self.tab_size))
+
+        elif key == QtCore.Qt.Key_Backspace:
+            if not text_cursor_selected_text and current_line_text.strip() == '' and text_cursor.atBlockEnd():
+                delete_block = len(current_line_text) % self.tab_size
+
+                if delete_block == 0:
+                    delete_block = self.tab_size
+
+                for i in range(delete_block):
+                    super().keyPressEvent(event)
+            else:
+                super().keyPressEvent(event)
+
+        elif key == QtCore.Qt.Key_Return:
+            leading_spaces = len(current_line_text) - len(current_line_text.lstrip())
+
+            if not text_cursor_selected_text:
+                cursor_position = text_cursor.position() - 1
+
+                if cursor_position >= 0:
+                    text_at_cursor = self.toPlainText()[cursor_position]
+
+                    if text_at_cursor in ['{', '(', '[']:
+                        self.insertPlainText('\n\n')
+                        self.insertPlainText(''.ljust(leading_spaces))
+
+                        text_cursor.movePosition(text_cursor.Up)
+                        self.setTextCursor(text_cursor)
+
+                        leading_spaces += self.tab_size
+                    else:
+                        super().keyPressEvent(event)
+                else:
+                    super().keyPressEvent(event)
+            else:
+                super().keyPressEvent(event)
+
+            self.insertPlainText(''.ljust(leading_spaces))
+
+        elif key == QtCore.Qt.Key_BraceLeft:
+            self._insert_matching_delimiter('{')
+        elif key == QtCore.Qt.Key_BraceRight:
+            if not self._handle_cursor_behavior('}'):
+                self.insertPlainText('}')
+
+        elif key == QtCore.Qt.Key_ParenLeft:
+            self._insert_matching_delimiter('(')
+        elif key == QtCore.Qt.Key_ParenRight:
+            if not self._handle_cursor_behavior(')'):
+                self.insertPlainText(')')
+
+        elif key == QtCore.Qt.Key_BracketLeft:
+            self._insert_matching_delimiter('[')
+        elif key == QtCore.Qt.Key_BracketRight:
+            if not self._handle_cursor_behavior(']'):
+                self.insertPlainText(']')
+
+        elif key == QtCore.Qt.Key_QuoteDbl:
+            if not self._handle_cursor_behavior('"'):
+                self._insert_matching_delimiter('"')
+
+        elif key == QtCore.Qt.Key_Apostrophe:
+            if not self._handle_cursor_behavior('\''):
+                self._insert_matching_delimiter('\'')
         else:
             super().keyPressEvent(event)
 
